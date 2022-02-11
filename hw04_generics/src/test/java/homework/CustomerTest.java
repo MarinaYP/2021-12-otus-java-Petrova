39844f5da2959b16a6e83aa11f990dfb28265ecc
Marina Petrova <marinayp@yandex.ru>
2022-02-11 18:31:00 +0300

    home_work to lesson 4. Test 1 passed

--- hw04_generics/src/test/java/homework/CustomerTest.java
+++ hw04_generics/src/test/java/homework/CustomerTest.java
@@ -29,6 +29,7 @@ class CustomerTest {
     }
 
     @Test
+    //@Disabled //надо удалить
     @DisplayName("Объект Customer как ключ в карте")
     void customerAsKeyTest() {
         //given
@@ -56,6 +57,7 @@ class CustomerTest {
     }
 
     @Test
+    @Disabled //надо удалить
     @DisplayName("Сортировка по полю score, итерация по возрастанию")
     void scoreSortingTest() {
         //given
@@ -95,6 +97,7 @@ class CustomerTest {
     }
 
     @Test
+    @Disabled //надо удалить
     @DisplayName("Модификация коллекции")
     void mutationTest() {
         //given
@@ -116,6 +119,7 @@ class CustomerTest {
     }
 
     @Test
+    @Disabled //надо удалить
     @DisplayName("Возвращание в обратном порядке")
     void reverseOrderTest() {
         //given
