fb889aeb42f011a69868e58165ee954e2cec8e73
Marina Petrova <marinayp@yandex.ru>
2022-03-14 23:53:58 +0300

    home_work to lesson 6.

--- hw06_annotation/src/main/java/ru.otus/reflection/MyReflection.java
+++ hw06_annotation/src/main/java/ru.otus/reflection/MyReflection.java
@@ -1,6 +1,8 @@
 package ru.otus.reflection;
 
+import ru.otus.annotations.After;
 import ru.otus.annotations.Before;
+import ru.otus.annotations.Test;
 
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Constructor;
@@ -10,9 +12,22 @@ import java.util.Arrays;
 public class MyReflection {
     public static void main(String[] args) throws Exception{
         runTests("ru.otus.reflection.MyCats");
+       // runTests("ru.otus.retention.MyPets");
+        runTests("ru.otus.reflection.MyDogs");
     }
 
     private static void runTests(String className) throws Exception{
+        try
+        { oneTest(className);}
+        catch(Exception ex) {System.out.println(ex.getMessage());}
+    }
+
+
+
+    private static void oneTest(String className) throws Exception{
+       Method methodBefore = null;
+       Method methodTest = null;
+       Method methodAfter = null;
         Class<?> clazz = Class.forName(className);
         System.out.println("Class Name:" + clazz.getSimpleName());
         System.out.println("canonicalName:" + clazz.getCanonicalName());
@@ -25,17 +40,25 @@ public class MyReflection {
 
         Method[] methods = clazz.getDeclaredMethods();
         for (Method method:methods){
-            Annotation[] annotations = method.getDeclaredAnnotations();
-            System.out.println("Method: "+ method.toString() + ", Annotations:" + Arrays.toString(annotations));
-            for (Annotation annotation:annotations){
-                ;
-            }
+            //Annotation[] annotations = method.getDeclaredAnnotations();
+            Annotation annotationBefore = method.getDeclaredAnnotation(Before.class);
+            if (annotationBefore != null) {methodBefore = method;
+            System.out.println("MethodBefore="+methodBefore.toString());}
+
+            Annotation annotationTest = method.getDeclaredAnnotation(Test.class);
+            if (annotationTest != null) {methodTest = method;
+                System.out.println("MethodTest="+methodTest.toString());}
+
+            Annotation annotationAfter = method.getDeclaredAnnotation(After.class);
+            if (annotationAfter != null) {methodAfter = method;
+                System.out.println("MethodAfter="+methodAfter.toString());}
+            //System.out.println("Method: "+ method.toString() + ", Annotations:" + Arrays.toString(annotations));
         }
-
-
-        //System.out.println("Methods:" + Arrays.toString(methods));
-        //Method[] methods = clazz.getDeclaredMethods();
-        //System.out.println(Arrays.toString(methods));
-        //System.out.println(Arrays.toString(annotations));
+        //System.out.println("MethodBefore="+methodBefore.toString());
+        //System.out.println("MethodTest="+methodTest.toString());
+        //System.out.println("MethodAfter="+methodAfter.toString());
+        methodBefore.invoke(object);
+        methodTest.invoke(object, 5);
+        methodAfter.invoke(object);
     }
 }
